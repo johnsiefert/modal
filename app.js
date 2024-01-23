fca53b5755f2d70150e692f2a91fb402734a5c8e
John <johnsief12@gmail.com>
2024-01-23 16:38:26 -0500

    added open modal and close button dynamically

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const button = document.querySelector('.modal-btn');
+const close = document.querySelector('.close-btn');
+const overlay = document.querySelector('.modal-overlay');
+
+
+function open(){
+ overlay.classList.add('open-modal');
+}
+
+function closeBtn(){
+    overlay.classList.remove('open-modal')
+}
+
+button.addEventListener('click', open)
+close.addEventListener('click', closeBtn);
